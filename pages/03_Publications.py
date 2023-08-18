893058868cabfd44139741ccbeddeb803e93e6bf
Sebastian Flores <sebastiandres@gmail.com>
2023-08-18 09:19:22 -0400

    pubs

---
+++ pages/03_Publications.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title("Publications")
+
+st.markdown("""Available equations:
+* [Pub1](https://www.google.com): 200X/03/01 - bleblebele
+* [Pub1](https://www.google.com): 200X/03/01 - bleblebele
+* [Pub1](https://www.google.com): 200X/03/01 - bleblebele
+* [Pub1](https://www.google.com): 200X/03/01 - bleblebele
+* [Pub1](https://www.google.com): 200X/03/01 - bleblebele
+* [Pub1](https://www.google.com): 200X/03/01 - bleblebele
+""")
