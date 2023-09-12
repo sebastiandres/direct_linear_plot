43ad9b15cdc1e61db6afc82244d3de7d044502c6
Sebastian Flores <sebastiandres@gmail.com>
2023-09-11 21:55:33 -0300

    updated plot with better alpha

--- pages/02_Error_Analysis.py
+++ pages/02_Error_Analysis.py
@@ -60,6 +60,7 @@ if eqn_sel == "Michaelis-Menten":
         opt_sel = st.radio("Select option", ["Plot estimations", "Plot errors"], horizontal=True)
         DLP = st.session_state["DLP"]
         LR = st.session_state["LR"]
+        alpha = (1/N)**0.25
         if opt_sel=="Plot estimations":
             # Plot the results on a graph
             fig, ax = plt.subplots(1, 2, figsize=(12, 6))
@@ -74,7 +75,7 @@ if eqn_sel == "Michaelis-Menten":
             ax[0].plot([-xymax, xymax*1.1], [-xymax, xymax*1.1], "k", alpha=0.25)
             ax[0].plot([-xymax, Vmax], [Vmax, Vmax], "r", alpha=0.5)
             ax[0].plot([Vmax, Vmax], [-xymax, Vmax], "r", alpha=0.5)
-            ax[0].plot(DLP["Vmax"], LR["Vmax"], "o", label="Vmax", alpha=0.25)
+            ax[0].plot(DLP["Vmax"], LR["Vmax"], "o", label="Vmax", alpha=alpha)
             ax[0].set_xlim(xmin, xmax)
             ax[0].set_ylim(xmin, xmax)
             # Second plot
@@ -85,7 +86,7 @@ if eqn_sel == "Michaelis-Menten":
             xymax = max(xmax, ymax)
             ax[1].set_xlabel("Kmax estimated using Direct Linear Plot")
             ax[1].set_ylabel("Kmax estimated using Linear Regression")
-            ax[1].plot([-xymax, xymax*1.1], [-xymax, xymax*1.1], "k", alpha=0.25)
+            ax[1].plot([-xymax, xymax*1.1], [-xymax, xymax*1.1], "k", alpha=alpha)
             ax[1].plot([-xymax, Km], [Km, Km], "r", alpha=0.5)
             ax[1].plot([Km, Km], [-xymax, Km], "r", alpha=0.5)
             ax[1].set_xlim(xmin, xmax)
@@ -110,8 +111,8 @@ if eqn_sel == "Michaelis-Menten":
             y = np.abs(LR["Vmax"]-Vmax)
             m = x < y # DLP < LR means DLP is more precise
             xmax = max(x.max(), y.max())
-            ax[0].plot(x[m], y[m], "gs", label="Vmax", alpha=0.25)
-            ax[0].plot(x[~m], y[~m], "bs", label="Vmax", alpha=0.25)
+            ax[0].plot(x[m], y[m], "gs", label="Vmax", alpha=alpha)
+            ax[0].plot(x[~m], y[~m], "bs", label="Vmax", alpha=alpha)
             ax[0].set_xlabel("Absolute error in Vmax estimated using Direct Linear Plot")
             ax[0].set_ylabel("Absolute error in Vmax estimated using Linear Regression")
             ax[0].plot([0, xmax], [0, xmax], "k", alpha=0.25)
@@ -122,11 +123,14 @@ if eqn_sel == "Michaelis-Menten":
             x = np.abs(DLP["Km"]-Km)
             y = np.abs(LR["Km"]-Km)
             m = x < y # DLP < LR means DLP is more precise
-            ax[1].plot(x[m], y[m], "gs", label="Km", alpha=0.25)
-            ax[1].plot(x[~m], y[~m], "bs", label="Km", alpha=0.25)
+            xmax = max(x.max(), y.max())
+            ax[1].plot(x[m], y[m], "gs", label="Km", alpha=alpha)
+            ax[1].plot(x[~m], y[~m], "bs", label="Km", alpha=alpha)
             ax[1].set_xlabel("Absolute error in Km estimated using Direct Linear Plot")
             ax[1].set_ylabel("Absolute error in Km estimated using Linear Regression")
             ax[1].set_title(f"Km: DLP is more precise than LR \n in {m.sum()}/{N} = {100*m.sum()/N}% of experiments")
+            ax[1].set_xlim(0, xmax)
+            ax[1].set_ylim(0, xmax)
             # Show the plot
             c1, c2 = st.columns([2,1])
             c1.pyplot(fig)
